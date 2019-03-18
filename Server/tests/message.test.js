7d8e00f7135f5bd9b72e4e410b06aa42cab50dde
Yves Ndagijimana <yvesndagije@gmail.com>
2019-03-17 23:54:44 -1000

    [Feature #164635859] Adding messages testing tools

--- Server/tests/message.test.js
+++ Server/tests/message.test.js
@@ -17,7 +17,7 @@ describe('Message Creation', () => {
     it('Should validate information', (done) => {
       chai.request(app)
         .post('/api/v1/messages/')
-        .set('token','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNTUyNTY1ODc1fQ.In5qfM-yH8P5DSF7fRh63oswK98bpiOZqPmP_WLg8w4')
+        .set('token','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MCwiaWF0IjoxNTUyODk5MzkwfQ.6XlR857ypV-lPbFRQt_pIYSESEZciSOeOUyiJ8rMlvM')
         .type('form')
         .send({email: 'ybb', subject: ''})
         .end((error, result) => {
@@ -29,7 +29,8 @@ describe('Message Creation', () => {
     it('Should send message if all require meet', (done) => {
       chai.request(app)
         .post('/api/v1/messages/')
-        .set('token','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNTUyNTY1ODc1fQ.In5qfM-yH8P5DSF7fRh63oswK98bpiOZqPmP_WLg8w4')
+        .set('token','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MCwiaWF0IjoxNTUyODk5MzkwfQ.6XlR857ypV-lPbFRQt_pIYSESEZciSOeOUyiJ8rMlvM')
+        .type('form')
         .send({email: 'yvesndagije@gmail.com', subject: 'Hi', message: 'This is to say hi'})
         .end((error, result) => {
           result.body.should.have.property('status').with.equal(200);
@@ -39,7 +40,8 @@ describe('Message Creation', () => {
     it('Should return a message object when send message', (done) => {
       chai.request(app)
         .post('/api/v1/messages/')
-        .set('token','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNTUyNTY1ODc1fQ.In5qfM-yH8P5DSF7fRh63oswK98bpiOZqPmP_WLg8w4')
+        .type('form')
+        .set('token','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MCwiaWF0IjoxNTUyODk5MzkwfQ.6XlR857ypV-lPbFRQt_pIYSESEZciSOeOUyiJ8rMlvM')
         .send({email: 'yvesndagije@gmail.com', subject: 'Hi', message: 'This is to say hi'})
         .end((error, result) => {
           result.body.should.have.property('status').with.equal(200);
@@ -66,7 +68,7 @@ describe('Reading inbox', () => {
   it('Should send message if all require meet', (done) => {
     chai.request(app)
       .get('/api/v1/messages/')
-      .set('token','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNTUyNTY1ODc1fQ.In5qfM-yH8P5DSF7fRh63oswK98bpiOZqPmP_WLg8w4')
+      .set('token','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MCwiaWF0IjoxNTUyODk5MzkwfQ.6XlR857ypV-lPbFRQt_pIYSESEZciSOeOUyiJ8rMlvM')
       .end((error, result) => {
         result.body.should.have.property('status').with.equal(200);
         done();
@@ -75,11 +77,240 @@ describe('Reading inbox', () => {
   it('Should get array containing message element', (done) => {
     chai.request(app)
       .get('/api/v1/messages/')
-      .set('token','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNTUyNTcyMjg5fQ.kYERJFk3SBBE4OB8vsT8mlfozKdIqxnO4WGQ3rz8Ng0')
+      .set('token','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MCwiaWF0IjoxNTUyODk5MzkwfQ.6XlR857ypV-lPbFRQt_pIYSESEZciSOeOUyiJ8rMlvM')
       .end((error, result) => {
         result.body.should.have.property('status').with.equal(200);
         result.body.should.have.property('data').to.not.be.empty;
         done();
       });
+  });
+  it('Should return a message object', (done) => {
+    chai.request(app)
+      .get('/api/v1/messages/')
+      .set('token','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MCwiaWF0IjoxNTUyODk5MzkwfQ.6XlR857ypV-lPbFRQt_pIYSESEZciSOeOUyiJ8rMlvM')
+      .end((error, result) => {
+        result.body.should.have.property('status').with.equal(200);
+        result.body.should.have.nested.property('data[0].id');
+        result.body.should.have.nested.property('data[0].message');
+        result.body.should.have.nested.property('data[0].subject');
+        result.body.should.have.nested.property('data[0].createdOn');
+        result.body.should.have.nested.property('data[0].status');
+        done();
+      });
+  });
+});
+
+describe('Reading unread', () => {
+  it('Should not read message if not authenticated', (done) => {
+    chai.request(app)
+      .get('/api/v1/messages/unread')
+      .end((error, result) => {
+        result.body.should.have.property('status').with.equal(401);
+        result.body.should.have.property('errors').with.lengthOf(1);
+        done();
+      });
+  });
+  it('Should send message if all require meet', (done) => {
+    chai.request(app)
+      .get('/api/v1/messages/unread')
+      .set('token','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MCwiaWF0IjoxNTUyODk5MzkwfQ.6XlR857ypV-lPbFRQt_pIYSESEZciSOeOUyiJ8rMlvM')
+      .end((error, result) => {
+        result.body.should.have.property('status').with.equal(200);
+        done();
+      });
+  });
+  it('Should get array containing message element', (done) => {
+    chai.request(app)
+      .get('/api/v1/messages/unread')
+      .set('token','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MCwiaWF0IjoxNTUyODk5MzkwfQ.6XlR857ypV-lPbFRQt_pIYSESEZciSOeOUyiJ8rMlvM')
+      .end((error, result) => {
+        result.body.should.have.property('status').with.equal(200);
+        result.body.should.have.property('data').to.not.be.empty;
+        done();
+      });
+  });
+  it('Should return a message object', (done) => {
+    chai.request(app)
+      .get('/api/v1/messages/unread')
+      .set('token','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MCwiaWF0IjoxNTUyODk5MzkwfQ.6XlR857ypV-lPbFRQt_pIYSESEZciSOeOUyiJ8rMlvM')
+      .end((error, result) => {
+        result.body.should.have.property('status').with.equal(200);
+        result.body.should.have.nested.property('data[0].id');
+        result.body.should.have.nested.property('data[0].message');
+        result.body.should.have.nested.property('data[0].subject');
+        result.body.should.have.nested.property('data[0].createdOn');
+        result.body.should.have.nested.property('data[0].status');
+        done();
+      });
+  });
+});
+describe('Reading draft', () => {
+  it('Should not read message if not authenticated', (done) => {
+    chai.request(app)
+      .get('/api/v1/messages/draft')
+      .end((error, result) => {
+        result.body.should.have.property('status').with.equal(401);
+        result.body.should.have.property('errors').with.lengthOf(1);
+        done();
+      });
+  });
+  it('Should send message if all require meet', (done) => {
+    chai.request(app)
+      .get('/api/v1/messages/draft')
+      .set('token','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MCwiaWF0IjoxNTUyODk5MzkwfQ.6XlR857ypV-lPbFRQt_pIYSESEZciSOeOUyiJ8rMlvM')
+      .end((error, result) => {
+        result.body.should.have.property('status').with.equal(200);
+        done();
+      });
+  });
+  it('Should get array containing message element', (done) => {
+    chai.request(app)
+      .get('/api/v1/messages/draft')
+      .set('token','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MCwiaWF0IjoxNTUyODk5MzkwfQ.6XlR857ypV-lPbFRQt_pIYSESEZciSOeOUyiJ8rMlvM')
+      .end((error, result) => {
+        result.body.should.have.property('status').with.equal(200);
+        result.body.should.have.property('data').to.not.be.empty;
+        done();
+      });
+  });
+  it('Should return a message object', (done) => {
+    chai.request(app)
+      .get('/api/v1/messages/draft')
+      .set('token','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MCwiaWF0IjoxNTUyODk5MzkwfQ.6XlR857ypV-lPbFRQt_pIYSESEZciSOeOUyiJ8rMlvM')
+      .end((error, result) => {
+        result.body.should.have.property('status').with.equal(200);
+        result.body.should.have.nested.property('data[0].id');
+        result.body.should.have.nested.property('data[0].message');
+        result.body.should.have.nested.property('data[0].subject');
+        result.body.should.have.nested.property('data[0].createdOn');
+        result.body.should.have.nested.property('data[0].status');
+        done();
+      });
+  });
+});
+describe('Deteting message', () => {
+  it('Should not read message if not authenticated', (done) => {
+    chai.request(app)
+      .delete('/api/v1/messages/1')
+      .end((error, result) => {
+        result.body.should.have.property('status').with.equal(401);
+        result.body.should.have.property('errors').with.lengthOf(1);
+        done();
+      });
+  });
+  it('Should delete message if all require meet', (done) => {
+    chai.request(app)
+      .delete('/api/v1/messages/1')
+      .set('token','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MCwiaWF0IjoxNTUyODk5MzkwfQ.6XlR857ypV-lPbFRQt_pIYSESEZciSOeOUyiJ8rMlvM')
+      .end((error, result) => {
+        result.body.should.have.property('status').with.equal(200);
+        done();
+      });
+  });
+  it('Should not delete message which does not exist', (done) => {
+    chai.request(app)
+      .delete('/api/v1/messages/3')
+      .set('token','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MCwiaWF0IjoxNTUyODk5MzkwfQ.6XlR857ypV-lPbFRQt_pIYSESEZciSOeOUyiJ8rMlvM')
+      .end((error, result) => {
+        result.body.should.have.property('status').with.not.equal(200);
+        done();
+      });
+  });
+  describe('Reading sent message', () => {
+    it('Should not read message if not authenticated', (done) => {
+      chai.request(app)
+        .get('/api/v1/messages/sent')
+        .end((error, result) => {
+          result.body.should.have.property('status').with.equal(401);
+          result.body.should.have.property('errors').with.lengthOf(1);
+          done();
+        });
+    });
+    it('Should send message if all require meet', (done) => {
+      chai.request(app)
+        .get('/api/v1/messages/sent')
+        .set('token','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MCwiaWF0IjoxNTUyODk5MzkwfQ.6XlR857ypV-lPbFRQt_pIYSESEZciSOeOUyiJ8rMlvM')
+        .end((error, result) => {
+          result.body.should.have.property('status').with.equal(200);
+          done();
+        });
+    });
+    it('Should get array containing message element', (done) => {
+      chai.request(app)
+        .get('/api/v1/messages/sent')
+        .set('token','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MCwiaWF0IjoxNTUyODk5MzkwfQ.6XlR857ypV-lPbFRQt_pIYSESEZciSOeOUyiJ8rMlvM')
+        .end((error, result) => {
+          result.body.should.have.property('status').with.equal(200);
+          result.body.should.have.property('data').to.not.be.empty;
+          done();
+        });
+    });
+    it('Should return a message object', (done) => {
+      chai.request(app)
+        .get('/api/v1/messages/sent')
+        .set('token','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MCwiaWF0IjoxNTUyODk5MzkwfQ.6XlR857ypV-lPbFRQt_pIYSESEZciSOeOUyiJ8rMlvM')
+        .end((error, result) => {
+          result.body.should.have.property('status').with.equal(200);
+          result.body.should.have.nested.property('data[0].id');
+          result.body.should.have.nested.property('data[0].message');
+          result.body.should.have.nested.property('data[0].subject');
+          result.body.should.have.nested.property('data[0].createdOn');
+          result.body.should.have.nested.property('data[0].status');
+          done();
+        });
+    });
+  });
+  describe('Reading Single message', () => {
+    it('Should not read message if not authenticated', (done) => {
+      chai.request(app)
+        .get('/api/v1/messages/0')
+        .end((error, result) => {
+          result.body.should.have.property('status').with.equal(401);
+          result.body.should.have.property('errors').with.lengthOf(1);
+          done();
+        });
+    });
+    it('Should get message if all require meet', (done) => {
+      chai.request(app)
+        .get('/api/v1/messages/0')
+        .set('token','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MCwiaWF0IjoxNTUyODk5MzkwfQ.6XlR857ypV-lPbFRQt_pIYSESEZciSOeOUyiJ8rMlvM')
+        .end((error, result) => {
+          result.body.should.have.property('status').with.equal(200);
+          done();
+        });
+    });
+    it('Should get array containing message element', (done) => {
+      chai.request(app)
+        .get('/api/v1/messages/0')
+        .set('token','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MCwiaWF0IjoxNTUyODk5MzkwfQ.6XlR857ypV-lPbFRQt_pIYSESEZciSOeOUyiJ8rMlvM')
+        .end((error, result) => {
+          result.body.should.have.property('status').with.equal(200);
+          result.body.should.have.property('data').to.not.be.empty;
+          done();
+        });
+    });
+    it('Should return a message object', (done) => {
+      chai.request(app)
+        .get('/api/v1/messages/0')
+        .set('token','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MCwiaWF0IjoxNTUyODk5MzkwfQ.6XlR857ypV-lPbFRQt_pIYSESEZciSOeOUyiJ8rMlvM')
+        .end((error, result) => {
+          result.body.should.have.property('status').with.equal(200);
+          result.body.should.have.nested.property('data[0].id');
+          result.body.should.have.nested.property('data[0].message');
+          result.body.should.have.nested.property('data[0].subject');
+          result.body.should.have.nested.property('data[0].createdOn');
+          result.body.should.have.nested.property('data[0].status');
+          done();
+        });
+    });
+    it('Should get error if message does not exist', (done) => {
+      chai.request(app)
+        .get('/api/v1/messages/3')
+        .set('token','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MCwiaWF0IjoxNTUyODk5MzkwfQ.6XlR857ypV-lPbFRQt_pIYSESEZciSOeOUyiJ8rMlvM')
+        .end((error, result) => {
+          result.body.should.have.property('status').with.not.equal(200);
+          done();
+        });
+    });
   });
 });
